3f98227b606836a42fa13cd18584c096890cbea6
Ryan Ishimaru <rishimaru23@gmail.com>
2021-08-03 20:57:40 -0700

    comments endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -118,4 +118,25 @@ router.put('/:id', (req, res) => {
     }
 })
 
+router.get("/:id/comments", (req, res) => {
+	Post.findPostComments(req.params.id)
+		.then((comments) => {
+			console.log(comments);
+			if (comments.length) {
+				res.status(200).json(comments);
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist",
+				});
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "The comments information could not be retrieved",
+			});
+		});
+});
+
+
 module.exports = router

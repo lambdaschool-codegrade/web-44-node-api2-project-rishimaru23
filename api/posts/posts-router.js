f9c580e6a8e7c14fc5db797f267a0ddcba415b02
Ryan Ishimaru <rishimaru23@gmail.com>
2021-08-03 20:28:33 -0700

    delete endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,4 +61,24 @@ router.post('/', (req, res) => {
     }
 })
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const post =  await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: "The post with specified ID does not exist",
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+            }   
+        } catch(err) {
+            res.status(500).json({
+                message: "The post could not be removed",
+                err: err.message,
+                stack: err.stack,
+            })
+        }
+})
+
 module.exports = router
